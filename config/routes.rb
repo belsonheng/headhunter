8057c44dcfa229d75128ca6280840cc657ca0301
Tiffany <tiffanyhqy15@hotmail.com>
2016-06-07 01:13:49 -0700

    added routes to 2 pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root  'pages#index'
 
   get   'pages/index'
+  get 'pages/employer'  => 'pages#employer'
+  get 'pages/howitworks' => 'pages#how_it_works'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
