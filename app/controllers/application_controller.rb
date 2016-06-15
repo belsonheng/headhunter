c58e2230c1a62406da55e9393b1e910e859ebdf5
Belson <belsonheng@hotmail.com>
2016-06-15 14:30:06 +0800

    added placeholder for loadtweets method def

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :load_tweets
+
+  private
+  def load_tweets
+  	@tweets = "my recent 3 tweets here"
+  end
 end
