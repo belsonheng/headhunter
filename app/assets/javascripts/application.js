3d2b8cf741402caf0874acd0fdf4f2d63e65a874
GabrielWoo <gabriel.woo97@gmail.com>
2016-06-21 00:06:06 -0700

    Added new Requirements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require jquery-2.1.4.min
 //= require jquery.migrate
 //= require modernizrr
