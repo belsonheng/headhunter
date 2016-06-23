1f289fd1642b3dc1d2960921d8207a11c5e3c74c
GabrielWoo <gabriel.woo97@gmail.com>
2016-06-22 19:07:23 -0700

    Added method to redirect Sign In

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,17 @@ def configure_permitted_parameters
   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
   devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :email, :password, :password_confirmation, :current_password) }
 end
+
+def after_sign_in_path_for(resource_or_scope)
+  if @user.type == "JobSeeker"
+    ## Path for JobSeeker
+ how_it_works_path
+ # Will Change Route 
+else
+  ## Path for Employers
+employers_path
+# Will Change Route 
+end
+
+end
 end
