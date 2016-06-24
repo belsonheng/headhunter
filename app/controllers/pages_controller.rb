9b94b7c8e35e6bad22d4239ddf2560dc23586d60
GabrielWoo <gabriel.woo97@gmail.com>
2016-06-24 01:14:03 -0700

    Added Download Method for Sitemap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,11 @@ class PagesController < ApplicationController
   end
   def sitemap
   	# return sitemap for download
+     send_file(
+    "#{Rails.root}/public/sitemap.xml.gz",
+    filename: "sitemap.xml.gz",
+    type: "application/xml"
+  )
   end
   def privacy
     @version = "1.0"
@@ -14,5 +19,9 @@ class PagesController < ApplicationController
     @host = "headhunter.sit.nyp.edu.sg"
   end
   def contact
+  end
+  def EHome
+  end 
+  def JHome
   end
 end
