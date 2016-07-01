72747f1c21dd2b5f98a1fed442426a973dd3c7da
Belson <belsonheng@hotmail.com>
2016-07-01 15:32:42 +0800

    update alignment

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,12 +13,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
-
-  def failure
-    redirect_to root_path
-  end
   
-
   def linkedin
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
@@ -34,17 +29,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def google
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-      if @user.persisted?
-        @user.skip_confirmation!
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in_and_redirect @user, :event => :authentication
-      else
-        flash[:notice] = "An error has occured, Please Register a new account or try again"
-        session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
+    if @user.persisted?
+      @user.skip_confirmation!
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      flash[:notice] = "An error has occured, Please Register a new account or try again"
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
   end
 end
