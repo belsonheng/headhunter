ef33f67b31769ec3fd14df5a34360a0f90266a73
Belson <belsonheng@hotmail.com>
2016-07-01 15:00:15 +0800

    fixed alignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,23 +16,20 @@ class ApplicationController < ActionController::Base
     @tweets = client.user_timeline("headhunter_nyp")
   end
 
-    protected
-def configure_permitted_parameters
-  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :name,:contact,:careerpath,:type,:email, :password, :password_confirmation, :company) }
-  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-  devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :email, :password, :password_confirmation, :current_password) }
-end
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :name,:contact,:careerpath,:type,:email, :password, :password_confirmation, :company) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :email, :password, :password_confirmation, :current_password) }
+  end
 
-def after_sign_in_path_for(resource_or_scope)
-  if @user.type == "JobSeeker"
-    ## Path for JobSeeker
- jobseeker_home_path
-
-else
-  ## Path for Employers
-employer_home_path
-
-end
-
-end
+  def after_sign_in_path_for(resource_or_scope)
+    if @user.type == "JobSeeker"
+      # Path for JobSeeker
+      jobseeker_home_path
+    else
+      # Path for Employers
+      employer_home_path
+    end
+  end
 end
