2a87a2c65d1bceaa6df496e090bd631f701e8d44
GabrielWoo <gabriel.woo97@gmail.com>
2016-07-04 01:52:36 -0700

    Updated User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,20 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable, :lockable, :confirmable,
+  # Resume
+
+  devise :database_authenticatable, :registerable, :omniauthable, :lockable, :confirmable, 
          :recoverable, :rememberable, :trackable, :validatable, :omniauth_providers => [:facebook, :linkedin, :google]
+
+##Resume
+has_one :document, dependent: :destroy
+
 
   ## Database authenticatable
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
   field :name,              type: String
-  field :contact,              type: String
+  field :location,              type: String
 
   ## Recoverable
   field :reset_password_token,   type: String
@@ -33,10 +39,12 @@ class User
    field :uid, type: String 
 
  #JobSeekers Attr.
- field :careerpath,              type: String
+ 
 
 #Employer Attr.
  field :company,              type: String
+  field :contact,              type: String
+
 
    
   ## Confirmable
@@ -49,6 +57,7 @@ class User
    field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
    field :unlock_token,    type: String # Only if unlock strategy is :email or :both
    field :locked_at,       type: Time
+
 def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid , type: "JobSeeker").first_or_create do |user|
     user.email = auth.info.email
