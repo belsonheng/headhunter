7f793cc12681e9799f0c5bbaeb1ab71e12c9a59b
GabrielWoo <gabriel.woo97@gmail.com>
2016-07-07 18:33:04 -0700

    Removed method for authentocate user (Devise already has it)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,16 +16,7 @@ class ApplicationController < ActionController::Base
     @tweets = client.user_timeline("headhunter_nyp")
   end
 
-  protected
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to new_user_session_path, :notice => 'Please Sign In'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
-  end
+ 
 
   protected
   def configure_permitted_parameters
